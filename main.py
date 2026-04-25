67968d7adfecca8460a586bb1ba2ccdfb7ed85e2
Maitret17 <tombieleg@gmail.com>
2026-04-25 19:15:43 +0200

    Added all remaining transportation test cases

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from algorithm import balashammer, northwest
 from display import print_matrix
 
 if __name__ == "__main__":
-    n, m, cost_matrix, cost_row, provision_column = parser("6")
+    n, m, cost_matrix, cost_row, provision_column = parser("10")
     print(northwest(n,m,cost_row,provision_column))
     print(balashammer(n,m,cost_matrix,cost_row,provision_column))
 
