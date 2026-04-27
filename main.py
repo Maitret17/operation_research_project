89417299348b592fab03620957c8758b9a4d72ba
Cider-Jude <jude.aybalen@gmail.com>
2026-04-27 17:11:55 +0200

    added the function to check wether the graph associated to the transport matrix is acyclic or not

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from parser import parser
-from algorithm import balashammer, northwest
+from algorithm import balashammer, northwest, acyclic
 from display import print_matrix
 
 if __name__ == "__main__":
-    n, m, cost_matrix, cost_row, provision_column = parser("6")
+    n, m, cost_matrix, cost_row, provision_column = parser("10")
     print(northwest(n,m,cost_row,provision_column))
+    print(acyclic(n, m, northwest(n,m,cost_row,provision_column)))
+    print("\n")
     print(balashammer(n,m,cost_matrix,cost_row,provision_column))
-
-    print(print_matrix(n, m, cost_matrix, provision_column, cost_row))
-
+    print(acyclic(n, m, balashammer(n,m,cost_matrix,cost_row,provision_column)))
+    print("\n")
+    print(print_matrix(n, m, cost_matrix, provision_column, cost_row))  
